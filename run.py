5f2c34ec40aeb54855b6a6a071d11d9dbf5a9c80
Peter Hennessy <peternesta@hotmail.com>
2022-04-22 20:39:23 +0000

    initial commit and create word_list file, lives_stages file

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from word_list import word_list
+from lives_stages import lives_lost
